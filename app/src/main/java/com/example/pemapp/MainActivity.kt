d743c8bc058bb4f9992197d95340542c52fbab22
Mariam <mariam_ali1000@yahoo.de>
2021-06-08 00:11:01 +0200

    style login and registration fragments

--- app/src/main/java/com/example/pemapp/MainActivity.kt
+++ app/src/main/java/com/example/pemapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.pemapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
--- app/src/main/java/com/example/pemapp/fragments/login/loginFragment.kt
+++ app/src/main/java/com/example/pemapp/fragments/login/loginFragment.kt
@@ -1,29 +1,37 @@
 package com.example.pemapp.fragments.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.pemapp.R
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [loginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class loginFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Hide titleBar
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        view.login_button.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_discoverFragment)
+        }
+
+        view.enter_registration_button.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
+        }
+
+        return view
     }
+
 }
