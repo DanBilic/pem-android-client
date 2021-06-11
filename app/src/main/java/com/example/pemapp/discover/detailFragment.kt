f5d3fc750c98b2ba14b95963501ae95f4c03fe91
Mariam <mariam_ali1000@yahoo.de>
2021-06-11 23:54:20 +0200

    change design of discover, add BottomNavigationView, add navigation graphs

--- app/src/main/java/com/example/pemapp/discover/detailFragment.kt
+++ app/src/main/java/com/example/pemapp/discover/detailFragment.kt
@@ -14,7 +14,9 @@ class detailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        val view = inflater.inflate(R.layout.fragment_detail, container, false)
+
+        return view
     }
 
 }
--- app/src/main/java/com/example/pemapp/moments/MomentsActivity.kt
+++ app/src/main/java/com/example/pemapp/moments/MomentsActivity.kt
@@ -2,7 +2,10 @@ package com.example.pemapp.moments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.pemapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MomentsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
