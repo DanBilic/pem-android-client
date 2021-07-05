aa621d3d06ef58b18f9f399f3f6a19fbcdd9b50d
BarbaraSe <seidinger_barbara@gmx.de>
2021-07-05 21:18:22 +0200

    added icons, added recyclerView for appUsage, data Model for appUsage, adapter for appUsage

--- app/src/main/java/com/example/pemapp/ui/appUsage/AppUsageFragment.kt
+++ app/src/main/java/com/example/pemapp/ui/appUsage/AppUsageFragment.kt
@@ -1,27 +1,18 @@
 package com.example.pemapp.ui.appUsage
 
-import android.Manifest
-import android.app.AppOpsManager
-import android.app.AppOpsManager.OPSTR_GET_USAGE_STATS
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.os.Process.myUid
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
-import androidx.core.app.AppOpsManagerCompat
 import androidx.fragment.app.Fragment
 import com.example.pemapp.R
-import kotlinx.android.synthetic.main.fragment_app_usage.view.*
-import kotlinx.android.synthetic.main.fragment_profile.view.*
-
+import com.example.pemapp.ui.moments.MomentsAdapter
 
 class AppUsage : Fragment() {
+    private lateinit var appUsageAdapter: AppUsageAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,8 +28,7 @@ class AppUsage : Fragment() {
             startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
         }
 
-        view.button1.setOnClickListener {
-        }
+
         return view
     }
 
