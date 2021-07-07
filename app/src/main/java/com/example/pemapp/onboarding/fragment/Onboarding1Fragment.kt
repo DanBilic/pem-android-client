7cef09cf0cc3a79795af27c0558128e80ced4bfe
BarbaraSe <seidinger_barbara@gmx.de>
2021-07-07 15:09:06 +0200

    finished onboarding:make picture, select picture from gallery, added chip buttons in recycler view, changed theme to light

--- app/src/main/java/com/example/pemapp/onboarding/fragment/Onboarding1Fragment.kt
+++ app/src/main/java/com/example/pemapp/onboarding/fragment/Onboarding1Fragment.kt
@@ -8,25 +8,21 @@ import android.view.ViewGroup
 import android.widget.EditText
 import androidx.navigation.fragment.findNavController
 import com.example.pemapp.R
-import kotlinx.android.synthetic.main.fragment_onboarding3.view.*
-
+import kotlinx.android.synthetic.main.fragment_onboarding1.view.*
 
 class Onboarding1Fragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_onboarding1, container, false)
-
         val userName = view.findViewById<EditText>(R.id.userName).text
         val location = view.findViewById<EditText>(R.id.location).text
 
-        view.nextButton.setOnClickListener {
+        view.nextButton1.setOnClickListener {
             findNavController().navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment)
+            //Todo: save userName, location in Object
         }
-
         return view
     }
 }
