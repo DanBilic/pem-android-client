0a57918f6850e0c1eee71dba2fc54a7856a9ef84
BarbaraSe <seidinger_barbara@gmx.de>
2021-07-11 17:15:03 +0200

    mark textInputField mandatory if empty, deleted username from onboarding 1 (because username is in registrationFragment)

--- app/src/main/java/com/example/pemapp/onboarding/fragment/Onboarding1Fragment.kt
+++ app/src/main/java/com/example/pemapp/onboarding/fragment/Onboarding1Fragment.kt
@@ -1,6 +1,7 @@
 package com.example.pemapp.onboarding.fragment
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.EditText
 import androidx.navigation.fragment.findNavController
 import com.example.pemapp.R
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.fragment_onboarding1.view.*
 
 class Onboarding1Fragment : Fragment() {
@@ -16,13 +18,18 @@ class Onboarding1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_onboarding1, container, false)
-        val userName = view.findViewById<EditText>(R.id.userName).text
         val location = view.findViewById<EditText>(R.id.location).text
+        val locationLayout = view.findViewById<TextInputLayout>(R.id.locationTextField)
 
         view.nextButton1.setOnClickListener {
-            findNavController().navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment)
+            if (TextUtils.isEmpty(location)) {
+                locationLayout.setError(" Please Enter Your Location");
+            } else {
+                findNavController().navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment)
+            }
             //Todo: save userName, location in Object
         }
         return view
     }
 }
+
